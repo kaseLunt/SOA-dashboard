2d0d6193ff07927380dae624f371e1b756885648
kase <kllunt@asu.edu>
2023-09-28 23:07:49 +0000

    get WeatherService structure in place

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,8 @@
 ﻿using Client.StockBuildServiceRef;
+using Client.StockQuoteServiceRef;
+using Client.WeatherServiceRef;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Client
 {
@@ -12,29 +10,35 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            // Specify the stock symbol for testing
-            string stockSymbol = "AAPL"; // Change this to the symbol you want to test
+            // Prompt the user for a stock symbol
+            Console.Write("Enter the stock symbol: ");
+            string stockSymbol = Console.ReadLine();
 
-            // Invoke the StockBuild operation
+            // Invoke the StockBuild operation to store dummy stock price
             StockBuildServiceClient buildClient = new StockBuildServiceClient();
             string filePath = buildClient.StockBuild(stockSymbol);
 
-            // Check if the file exists
+            // Check if the file was created successfully
             if (File.Exists(filePath))
             {
                 Console.WriteLine("File created successfully.");
 
-                // Read and display the file content
-                string[] lines = File.ReadAllLines(filePath);
-                foreach (string line in lines)
-                {
-                    Console.WriteLine(line);
-                }
+                // Now invoke the StockQuote operation to fetch the stock price
+                StockQuoteServiceClient quoteClient = new StockQuoteServiceClient();
+                string stockPrice = quoteClient.StockQuote(stockSymbol);
+
+                Console.WriteLine($"Stock Price for {stockSymbol}: {stockPrice}");
             }
             else
             {
-                Console.WriteLine("File creation failed.");
+                Console.WriteLine("File creation failed or the service couldn't read the stock price.");
             }
+
+            // Invoke GetWeather operation 
+            WeatherServiceClient weatherClient = new WeatherServiceClient();
+            Console.WriteLine(weatherClient.GetWeather(85282));
+
+            Console.ReadKey();
         }
     }
 }
--- StockQuoteService/StockQuoteService.svc.cs
+++ StockQuoteService/StockQuoteService.svc.cs
@@ -1,18 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
-//StockQuoteService.svc.cs
+using System.IO;
+
 namespace StockQuoteService
 {
+    /// <summary>
+    /// Provides functionality to retrieve stock prices based on stock symbols.
+    /// </summary>
     public class StockQuoteService : IStockQuoteService
     {
+        // Path to the shared file containing stock symbols and their respective prices.
+        private static readonly string FILE_PATH;
+
+        /// <summary>
+        /// Static constructor to initialize the FILE_PATH.
+        /// Determines the path to the shared data file 'sharedData.txt' located in the 'Project3' directory.
+        /// </summary>
+        static StockQuoteService()
+        {
+            // Get the base directory of the currently executing application.
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Ascend to the StockQuoteService directory.
+            DirectoryInfo projectDirectoryInfo = Directory.GetParent(currentDirectory);
+
+            // Ascend to the Project3 (solution) directory.
+            DirectoryInfo solutionDirectoryInfo = Directory.GetParent(projectDirectoryInfo.FullName);
+
+            // Combine the solution directory path with the filename to get the full path to the shared file.
+            FILE_PATH = Path.Combine(solutionDirectoryInfo.FullName, "sharedData.txt");
+        }
+
+        /// <summary>
+        /// Retrieves the price of a given stock symbol.
+        /// </summary>
+        /// <param name="stock">The stock symbol for which the price needs to be fetched.</param>
+        /// <returns>A string indicating the price of the input stock symbol.</returns>
         public string StockQuote(string stock)
         {
-            return $"You entered: {stock.ToUpper()}";
+            // Default price message if the stock symbol is not found in the file.
+            string stockPrice = "Not Found";
+
+            // Check if the shared data file exists.
+            if (File.Exists(FILE_PATH))
+            {
+                // Read all lines from the shared data file.
+                string[] stockData = File.ReadAllLines(FILE_PATH);
+
+                // Iterate over each line in the data.
+                foreach (string line in stockData)
+                {
+                    // Split the line into symbol and price parts using the delimiter '|'.
+                    string[] parts = line.Split('|');
+
+                    // Check if the current line's symbol matches the input stock symbol.
+                    if (parts.Length == 2 && parts[0].Equals(stock, StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Assign the matched stock's price to the stockPrice variable.
+                        stockPrice = parts[1];
+                        break;
+                    }
+                }
+            }
+
+            // Return the stock price message.
+            return $"Price of {stock.ToUpper()}: {stockPrice}";
         }
     }
 }
